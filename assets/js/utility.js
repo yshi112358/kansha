4c5a38cf9410673f7237fd0119df91a517cad627
yshi112358 <yshi112358@gmail.com>
2022-09-27 21:27:35 +0900

    Update utility.js

--- assets/js/utility.js
+++ assets/js/utility.js
@@ -1,45 +1,45 @@
 var folder = "/assets/img/kansha/";
 console.log(location.hostname);
 
-if (location.hostname == "127.0.0.1") {
-    for (let n = 0; n < 5; n++) {
-        $.ajax({
-            url: folder,
-            success: function (data) {
-                $(data).find("a").attr("href", function (i, val) {
-                    if (val.match(/\.(jpe?g|png|gif)$/)) {
-                        $(".kansha-gallery").append("<img src='" + val + "'>");
-                    }
-                });
-            }
-        });
-    }
-}
-else if (location.hostname == "yshi112358.github.io") {
-    for (let n = 0; n < 5; n++) {
-        $.ajax({
-            url: folder,
-            success: function (data) {
-                $(data).find("a").attr("href", function (i, val) {
-                    if (val.match(/\.(jpe?g|png|gif)$/)) {
-                        $(".kansha-gallery").append("<img src='/kansha" + folder + val + "'>");
-                    }
-                });
-            }
-        });
-    }
-}
-else {
-    for (let n = 0; n < 5; n++) {
-        $.ajax({
-            url: folder,
-            success: function (data) {
-                $(data).find("a").attr("href", function (i, val) {
-                    if (val.match(/\.(jpe?g|png|gif)$/)) {
-                        $(".kansha-gallery").append("<img src='" + folder + val + "'>");
-                    }
-                });
-            }
-        });
-    }
-}
+// if (location.hostname == "127.0.0.1") {
+//     for (let n = 0; n < 5; n++) {
+//         $.ajax({
+//             url: folder,
+//             success: function (data) {
+//                 $(data).find("a").attr("href", function (i, val) {
+//                     if (val.match(/\.(jpe?g|png|gif)$/)) {
+//                         $(".kansha-gallery").append("<img src='" + val + "'>");
+//                     }
+//                 });
+//             }
+//         });
+//     }
+// }
+// else if (location.hostname == "yshi112358.github.io") {
+//     for (let n = 0; n < 5; n++) {
+//         $.ajax({
+//             url: folder,
+//             success: function (data) {
+//                 $(data).find("a").attr("href", function (i, val) {
+//                     if (val.match(/\.(jpe?g|png|gif)$/)) {
+//                         $(".kansha-gallery").append("<img src='/kansha" + folder + val + "'>");
+//                     }
+//                 });
+//             }
+//         });
+//     }
+// }
+// else {
+//     for (let n = 0; n < 5; n++) {
+//         $.ajax({
+//             url: folder,
+//             success: function (data) {
+//                 $(data).find("a").attr("href", function (i, val) {
+//                     if (val.match(/\.(jpe?g|png|gif)$/)) {
+//                         $(".kansha-gallery").append("<img src='" + folder + val + "'>");
+//                     }
+//                 });
+//             }
+//         });
+//     }
+// }
