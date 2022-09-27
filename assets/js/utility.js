21e5a961f7e399b77346a3bd3284b8fd327e60db
yshi112358 <yshi112358@gmail.com>
2022-09-27 21:18:46 +0900

    Update URL

--- assets/js/utility.js
+++ assets/js/utility.js
@@ -14,6 +14,20 @@ if (location.hostname == "127.0.0.1") {
         });
     }
 }
+else if (location.hostname == "yshi112358.github.io") {
+    for (let n = 0; n < 5; n++) {
+        $.ajax({
+            url: folder,
+            success: function (data) {
+                $(data).find("a").attr("href", function (i, val) {
+                    if (val.match(/\.(jpe?g|png|gif)$/)) {
+                        $(".kansha-gallery").append("<img src='/kansha" + folder + val + "'>");
+                    }
+                });
+            }
+        });
+    }
+}
 else {
     for (let n = 0; n < 5; n++) {
         $.ajax({
