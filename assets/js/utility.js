418681026411550e839587820781fbd51d695d7b
yshi112358 <yshi112358@gmail.com>
2022-09-27 20:58:57 +0900

    Update utility.js

--- assets/js/utility.js
+++ assets/js/utility.js
@@ -1,6 +1,6 @@
 var folder = "./assets/img/kansha/";
 
-if (location.hostname == "127.0.0.1") {
+if (location.hostname == "127.0.0.1" || location.hostname == "yshi112358.github.io") {
     for (let n = 0; n < 5; n++) {
         $.ajax({
             url: folder,
